d33d0ca43bd799c0e6f5c5bda97a274b132215f3
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-30 17:57:03 -0300

    chore: :sparkles: Inplementando o footer da pagina
    Implementando o footer da pagina com os links das redes sociais da wine

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,53 @@
 import React from "react";
-// import { useNavigate } from "react-router-dom";
+import FormPag from "../image/form-pag.svg";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { BsTwitter, BsYoutube } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <div>
-      <h1>To no Footer</h1>
+      <div>
+        <img
+          src={FormPag}
+          alt="Ilustrações das logos das empresas de cartões de credito"
+        />
+      </div>
+      <div>
+        <a
+          href="https://www.facebook.com/winevinhos/#xd_co_f=Y2RiN2VlYmQtMTJiZi00NDYyLWEyMDAtMDI5ZjAyMjdkYTJl~"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebookF />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://twitter.com/wine_vinhos"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.instagram.com/winevinhos/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.youtube.com/user/WineVinhos"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsYoutube />
+        </a>
+      </div>
     </div>
   );
 };
