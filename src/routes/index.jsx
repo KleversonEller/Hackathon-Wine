4e9461fcd8a6e4cc11a458322e96ee865639e898
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-30 12:56:04 -0300

    refactor: :recycle: Configuração das rotas
    Confingurando as rotas para o padrão da versão 6 do Router-Dom

---
+++ src/routes/index.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+const Routers = () => {
+  <BrowserRouter>
+    <Routes>
+      <Route></Route>
+    </Routes>
+  </BrowserRouter>;
+};
+
+export default Routers;
