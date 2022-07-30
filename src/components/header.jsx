61857a98de5be58f214cec2ef85ea712175d36f2
Tamiris Shigaki <shigakiendo@gmail.com>
2022-07-30 18:16:47 -0300

    refactor: :recycle: refatoração do codigo
    refatorando codgo para implementação do header e navBar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,11 +2,7 @@ import React from "react";
 // import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>
-      <h1>To no Header</h1>
-    </div>
-  );
+  return <div>{/* <h1>To no Header</h1> */}</div>;
 };
 
 export default Header;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,26 +2,55 @@ import React from "react";
 // import { useNavigate } from "react-router-dom";
 import Header from "../components/header";
 import NavBar from "../components/nav.bar";
-import Produtos from "../components/prdutos";
+import Produtos from "../components/produtos";
 import Insta from "../components/insta";
 import Planos from "../components/planos";
 import Curiosidades from "../components/curiosidades";
 import Footer from "../components/footer";
+import garrafas3 from "../image/garrafas3.jpeg";
+import garrafa2 from "../image/garrafa2.png";
+import logoWine from "../image/logoWine.png";
+import { MdPermIdentity } from "@react-icons/all-files/md/MdPermIdentity";
+import { BiCart } from "@react-icons/all-files/bi/BiCart";
+import "../css/header.css";
 
 const Home = () => {
   return (
     <div>
-      <div>
-        <Header />
-      </div>
+      <Header />
+      <header className="header-container">
+        <img src={garrafa2} alt="garrafas coloridas" className="header-img-bottle1"></img>
+        <img src={logoWine} alt="imagem logo wine" className="header-img-logo"></img>
+        <h1>O maior clube de vinhos do mundo</h1>
+        <img
+          src={garrafas3}
+          alt="garrafas coloridas"
+          className="header-img-bottle2"
+        ></img>
+      </header>
       <div>
         <NavBar />
+        <h3>Clube</h3>
+        <h3>Loja</h3>
+        <h3>Produtores</h3>
+        <h3>Ofertas</h3>
+        <h3>Eventos</h3>
+        <h3>Baixe o APP</h3>
+        <h3>
+          <MdPermIdentity />
+        </h3>
+        <h3>
+          <BiCart />
+        </h3>
       </div>
+      <section>
+        <Insta />
+        <div></div>
+        <div></div>
+        <div></div>
+      </section>
       <div>
         <Produtos />
-      </div>
-      <div>
-        <Insta />
       </div>
       <div>
         <Planos />
