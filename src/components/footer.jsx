3e1598b11f76b5b3d256ab70225eb758ef15858f
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-30 16:33:46 -0300

    feat: :sparkles: Criando componentes
    Criando os componentes que faram parte do Home

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>To no Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>To no Header</h1>
+    </div>
+  );
+};
+
+export default Header;
