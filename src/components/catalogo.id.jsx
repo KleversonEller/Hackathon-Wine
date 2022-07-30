906102ffb92585697f503ac16e42d4e56a8ebb00
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-30 15:03:07 -0300

    feat: :sparkles: Criando componentes
    Criando o componente "catalogo-id" que ira renderizar produtos pelo id

---
+++ src/components/catalogo.id.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+
+const CatalogoId = () => {
+  return (
+    <div>
+      <h1>To no catalago ID</h1>
+    </div>
+  );
+};
+
+export default CatalogoId;
