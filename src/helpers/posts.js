e51d6754c6ead4c936afff9f7b73323b0ad099bb
ottomicheletti <michelettiotto@gmail.com>
2022-07-30 23:08:29 -0300

    feat(instagramMock): :sparkles: Adiciona um DB local para mockar posts do Instagram com a hashtag #WineExperience.

---
+++ src/helpers/posts.js
@@ -0,0 +1,41 @@
+const posts = [
+  {
+    username: "sentalaclaudia",
+    img: "01",
+    caption: "Um champagne aos pés da torre Eiffel! #WineExperience",
+    localization: "Torre Eiffel - Paris, França",
+    comments: 23987,
+  },
+  {
+    username: "mufasa_",
+    img: "02",
+    caption: "Sextou com vinho e mozão! ❤️‍🔥 #WineExperience",
+    localization: "São Paulo",
+    comments: 143,
+  },
+  {
+    username: "bey81",
+    img: "03",
+    caption: `Concrete jungle where dreams are made of 🍷
+ #WineExperience #NewYork #EmpireState #Lifestyle`,
+    localization: "New York - USA",
+    comments: 1351,
+  },
+  {
+    username: "rogerinhodoinga",
+    img: "04",
+    caption: `Conhecendo vinícolas em Alentejo com uma degustação mais que especial.
+  Quer saber como chegar aqui? Link na bio. #WineExperience`,
+    localization: "Alentejo, Portugal",
+    comments: 15,
+  },
+  {
+    username: "silvio.trader",
+    img: "05",
+    caption: "Conhecendo Napa Valley com os amigos!!🛬🍷🍇 #WineExperience",
+    localization: "Napa Valley, California",
+    comments: undefined,
+  },
+];
+
+export default posts;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 // import { useNavigate } from "react-router-dom";
+import InstagramMock from "../components/InstagramMock";
 
 const Home = () => {
   return (
     <div>
       <h1>To no Home</h1>
+      <InstagramMock />
     </div>
   );
 };
