c01baf19e1e53414eb6ae2d5a63dab02bbf4ec4b
Thami <thami.lourenco@outlook.com>
2022-07-31 06:44:32 -0300

    Css planos

--- src/components/planos.jsx
+++ src/components/planos.jsx
@@ -4,6 +4,7 @@ import useStore from "../store";
 import { v4 as uuidv4 } from "uuid";
 import { FiGift } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
+import wineBottle from "../image/wineBottle.png";
 import "../css/planos.css";
 
 const Planos = () => {
@@ -18,33 +19,41 @@ const Planos = () => {
 
   return (
     <div className="plan-container">
-      <h3>Planos</h3>
-      {plansList.map((objeto) => (
-        <div key={uuidv4()}>
-          <span>{objeto.name}</span>
-          <br />
-          <span>{objeto.promotion}</span>
-          <br />
-          <span>
-            R$ <s>{objeto.price}</s> por
-          </span>
-          <br />
-          <span>R$ {objeto.priceWithDiscount} /mês</span>
-          <br />
-          {objeto.gift && (
+      <h3 className="plan-title">Planos</h3>
+      <div className="plan-objects">
+        <img src={wineBottle} alt="Imagem garrafa de vidro" className="plan-image" />
+        {plansList.map((objeto) => (
+          <div className="plan-card-promotion" key={uuidv4()}>
+            <span className="plan-card-name">{objeto.name}</span>
+            <br />
+            <br />
+            <span className="plan-card-promotion-price">{objeto.promotion}</span>
+            <br />
             <span>
-              <FiGift />
-              {objeto.gift}
+              R$ <s>{objeto.price}</s> por
             </span>
-          )}
-          <br />
-          <button type="button" onClick={() => navigate(`/planos/${objeto.id}`)}>
-            ASSINAR
-          </button>
-          <br />
-          <br />
-        </div>
-      ))}
+            <br />
+            <span>R$ {objeto.priceWithDiscount} /mês</span>
+            <br />
+            {objeto.gift && (
+              <span>
+                <FiGift />
+                {objeto.gift}
+              </span>
+            )}
+            <br />
+            <button
+              className="plan-btn"
+              type="button"
+              onClick={() => navigate(`/planos/${objeto.id}`)}
+            >
+              ASSINAR
+            </button>
+            <br />
+            <br />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
