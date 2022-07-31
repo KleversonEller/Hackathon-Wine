5f2caae8d36ebb27b63985804a5362c59a2d887a
Tamiris Shigaki <shigakiendo@gmail.com>
2022-07-31 01:34:22 -0300

    style: :lipstick: Adicionando css no arquivo footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,43 +9,49 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <img className="footer-image-bottle" src={garrafa4} alt="Garrafas coloridas" />
-      <div className="footer-socialMediaIcons">
-        <div>
-          <a
-            href="https://www.facebook.com/winevinhos/#xd_co_f=Y2RiN2VlYmQtMTJiZi00NDYyLWEyMDAtMDI5ZjAyMjdkYTJl~"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaFacebookF />
-          </a>
+      <div className="footer-itens">
+        <div className="footer-socialMediaIcons">
+          <div className="footer-mediaIcons">
+            <a
+              href="https://www.facebook.com/winevinhos/#xd_co_f=Y2RiN2VlYmQtMTJiZi00NDYyLWEyMDAtMDI5ZjAyMjdkYTJl~"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF />
+            </a>
+          </div>
+          <div className="footer-mediaIcons">
+            <a
+              href="https://twitter.com/wine_vinhos"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsTwitter />
+            </a>
+          </div>
+          <div className="footer-mediaIcons">
+            <a
+              href="https://www.instagram.com/winevinhos/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+          </div>
+          <div className="footer-mediaIcons">
+            <a
+              href="https://www.youtube.com/user/WineVinhos"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsYoutube />
+            </a>
+          </div>
         </div>
-        <div>
-          <a
-            href="https://twitter.com/wine_vinhos"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <BsTwitter />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://www.instagram.com/winevinhos/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaInstagram />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://www.youtube.com/user/WineVinhos"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <BsYoutube />
-          </a>
-        </div>
+        <p>
+          SE BEBER, NÃO DIRIJA. APRECIE COM MODERAÇÃO. A VENDA DE BEBIDAS ALCOÓLICAS É
+          PROIBIDA PARA MENORES DE 18 ANOS.
+        </p>
       </div>
       <img className="footer-image-bottle" src={garrafa5} alt="Garrafas coloridas" />
     </div>
